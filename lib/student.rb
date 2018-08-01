c2820e413148c8f43459d017a553ed5f1bccc716
Monica  Karasek <mlkarasek@gmail.com>
2018-08-01 09:27:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,7 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * FROM students WHERE name = ? DESC 1
+    SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
