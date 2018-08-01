288c8bf7d4eef4042c9a9e3ff2fe73790aae89d9
Monica  Karasek <mlkarasek@gmail.com>
2018-08-01 09:07:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,13 @@ SQL
         end
       end
 
+      def self.all_students_in_grade_X(grade)
+        SQL
+          SELECT * FROM students WHERE grade = ?
+              DB[:conn].execute(sql).map do |row|
+                self.new_from_db(row)
+              end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) VALUES (?, ?)
